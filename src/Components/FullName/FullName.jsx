76a4945801d39b46db42f377547b75b63d9570fe
Darshan <darshann679596@gmail.com>
2024-10-06 12:38:07 +0530

    Added update app and component Fullname

---
+++ src/Components/FullName/FullName.jsx
@@ -0,0 +1,30 @@
+import React,{useState} from 'react';
+const FullName = () =>{
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [fullName, setFullName] = useState('');  
+     
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(firstName,lastName)
+        setFullName(firstName + " " + lastName);
+        setIsSubmitted(true);
+    }
+    return(
+        <>
+            <form onSubmit = {handleSubmit}>
+                <label htmlFor = "firstName">First Name :</label>
+                <input type = "text" placeholder="" name ="First Name" id = "firstName" onChange ={(e) => setFirstName(e.target.value)} required/>
+                <br/>                
+                <label htmlFor = "lastName">Last Name :</label>
+                <input type = "text" placeholder="" name ="Last Name" id = "lastName" onChange = {(e) => setLastName(e.target.value)} required/>
+                <br/>
+                <button type = "submit">Submit</button>
+                {isSubmitted && <p>Full Name : {fullName}</p>}
+            </form>
+        </>
+    )
+}
+export default FullName;
