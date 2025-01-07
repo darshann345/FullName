011b0c82fa13fdb8f332386b41f4f0663548a739
Darshan <darshann679596@gmail.com>
2025-01-07 19:02:21 +0530

    code pushed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
-import './App.css';
-import FullName from './Components/FullName/FullName';
+import React,{useState,useEffect} from "react"
+const App = () =>{
+    const [firstName,setFirstName] = useState("")
+    const [lastName,setLastName] = useState("")
+    const [fullName,setFullName] = useState("")
+    const handleSubmit = (e) =>{
+        if(firstName.trim() === "" && lastName.trim() === ""){
+            e.preventDefault(); 
+            setFullName("")
+        }else{
+            setFullName(`${firstName} ${lastName}`)
+        }
+    }
+    return(
+        <>
+            <form>
+                <label>FirstName:</label>
+            <input 
+                type="text"
+                value={firstName}
+                onChange = {(e)=>{
+                    setFirstName(e.target.value)
+                }}
+                    required
+            />
+            <br/>
+            <label>LastName:</label>
+            <input 
+                type="text"
+                value={lastName}
+                onChange = {(e)=>{
+                    setLastName
+                        (e.target.value)
+                }}
+                required
+            />
+            <br/>
+            <button type = "submit" onClick={handleSubmit}>
+                Submit
+            </button>
+            </form>
 
+            {
+                fullName && (
+                    <>
+                        <p>Full Name: {fullName}</p>
+                    </>
+                )
+            }
+            
 
-
-function App() {
-  return (
-    <>
-      <FullName />
-
-    </>
-  );
+            
+        </>
+    )
 }
-
-export default App;
